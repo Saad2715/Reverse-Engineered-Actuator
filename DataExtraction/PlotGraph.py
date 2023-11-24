a9f4a49be096273923d466c67ac678581c461999
Saad2715 <msaadmunawar@gmail.com>
2023-11-24 22:42:44 +0400

    Graph Plots Complete
    Finalized and Completed code to Graphically visualize the sample data

--- DataExtraction/PlotGraph.py
+++ DataExtraction/PlotGraph.py
@@ -6,10 +6,10 @@ from scipy.interpolate import interp1d
 # ------------------- Experiment 1 File Paths ------------------- #
 EXPERIMENT_1_FILES_PATH = []
 
-EXP1SAM1PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 1\\Sample 1"
-EXP1SAM2PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 1\\Sample 2"
-EXP1SAM3PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 1\\Sample 3"
-EXP1SAM4PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 1\\Sample 4"
+EXP1SAM1PATH = "Experiment 1/Sample 1"
+EXP1SAM2PATH = "Experiment 1/Sample 2"
+EXP1SAM3PATH = "Experiment 1/Sample 3"
+EXP1SAM4PATH = "Experiment 1/Sample 4"
 
 EXPERIMENT_1_FILES_PATH.append(EXP1SAM1PATH)
 EXPERIMENT_1_FILES_PATH.append(EXP1SAM2PATH)
@@ -19,10 +19,10 @@ EXPERIMENT_1_FILES_PATH.append(EXP1SAM4PATH)
 # ------------------- Experiment 2 File Paths ------------------- #
 EXPERIMENT_2_FILES_PATH = []
 
-EXP2SAM1PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 2\\Sample 1"
-EXP2SAM2PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 2\\Sample 2 (Broke half way)"
-EXP2SAM3PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 2\\Sample 3 (break at 8mm & 9mm)"
-EXP2SAM4PATH = "C:\\Users\\msaad\\Documents\\Saad\\Research Projects\\Dr. Umer's Project\\Reverse-Engineered-Actuator\\DataExtraction\\Experiment 2\\Sample 4"
+EXP2SAM1PATH = "Experiment 2/Sample 1"
+EXP2SAM2PATH = "Experiment 2/Sample 2 (Broke half way)"
+EXP2SAM3PATH = "Experiment 2/Sample 3 (break at 8mm & 9mm)"
+EXP2SAM4PATH = "Experiment 2/Sample 4"
 
 EXPERIMENT_2_FILES_PATH.append(EXP2SAM1PATH)
 EXPERIMENT_2_FILES_PATH.append(EXP2SAM2PATH)
@@ -30,9 +30,157 @@ EXPERIMENT_2_FILES_PATH.append(EXP2SAM3PATH)
 EXPERIMENT_2_FILES_PATH.append(EXP2SAM4PATH)
 
 
-def PlotExp1Graph(FILEPATH, plotTitle):
+def PlotExp2Graph(FILEPATH, plotTitle):
+    Sample = {}  # Key will be displacement and value, the readings
+    displacementS2 = []
+    resistanceS2 = []
+    remappedDisp = []
+
+    for values in os.listdir(FILEPATH):
+        if len(values) == 7:
+            displacement_key = float(values[0])
+        elif len(values) == 8:
+            displacement_key = float(values[0:2])
+        elif len(values) == 9:
+            displacement_key = float(values[0:3])
+
+        if int(displacement_key) == displacement_key:
+            displacement_key = int(displacement_key)
+
+        Sample.setdefault(displacement_key, [])
+
+        with open(os.path.join(FILEPATH, values)) as file:
+            file_contents = [float(line.strip()) for line in file.readlines() if line.strip()]
+            Sample[displacement_key].extend(file_contents)
+
+    # Sort the dictionary items by key in ascending order
+    sorted_sample = dict(sorted(Sample.items()))
+
+    for key, value in sorted_sample.items():
+        length = len(value)
+        div = key / length
+
+        start_value = div
+        end_value = key
+        num_elements_forward = length // 2
+
+        # Create the list using numpy's arange function for forward displacement
+        temp1 = np.linspace(start_value, end_value, num_elements_forward).tolist()
+
+        # Now for backward displacement
+        start_value_backward = temp1[-1]
+        end_value_backward = 0
+        num_elements_backward = length - num_elements_forward
+
+        # Create the list using numpy's arange function for backward displacement
+        temp2 = np.linspace(start_value_backward, end_value_backward, num_elements_backward).tolist()
+
+        # Concatenate forward and backward displacement values
+        remappedDisp.extend(temp1 + temp2)
+
+    # Ensure the length of remappedDisp matches the length of resistanceS2
+    # remappedDisp = remappedDisp[:len(resistanceS2)]
+    for key, value in sorted_sample.items():
+        # print(key, value)
+        displacementS2.append(key)
+        resistanceS2.extend(value)
+
+    print(len(resistanceS2), len(remappedDisp))
+
+    # Adjust the length of remappedDisp to match the length of resistanceS2
+    remappedDisp = remappedDisp[:len(resistanceS2)]
+    print(len(remappedDisp))
+
+    x_values = np.array(remappedDisp)
+    y_values = np.array(resistanceS2)
+
+    plt.plot(x_values, y_values, label='Smoothed Curve', color='red')
+
+    # Set specific values for the x-axis ticks
+    plt.xticks(displacementS2)
+    plt.yticks(np.unique(resistanceS2))
+
+    plt.title(plotTitle)
+    plt.xlabel('Displacement')
+    plt.ylabel('Resistance')
+    plt.legend()
+    plt.show()
+
+
+def PlotExp2Interp(FILEPATH, plotTitle):
+    Sample = {}  # Key will be displacement and value, the readings
+    displacementS2 = []
+    resistanceS2 = []
+    remappedDisp = []
+
+    for values in os.listdir(FILEPATH):
+        # (Assuming your file name parsing logic remains the same)
+        if len(values) == 7:
+            displacement_key = float(values[0])
+        elif len(values) == 8:
+            displacement_key = float(values[0:2])
+        elif len(values) == 9:
+            displacement_key = float(values[0:3])
+
+        if int(displacement_key) == displacement_key:
+            displacement_key = int(displacement_key)
+
+        Sample.setdefault(displacement_key, [])
+
+        with open(os.path.join(FILEPATH, values)) as file:
+            file_contents = [float(line.strip()) for line in file.readlines() if line.strip()]
+            Sample[displacement_key].extend(file_contents)
+
+    # Sort the dictionary items by key in ascending order
+    sorted_sample = dict(sorted(Sample.items()))
+
+    for key, value in sorted_sample.items():
+        length = len(value)
+        div = key / length
+
+        start_value = div
+        end_value = key
+        num_elements_forward = length // 2
+
+        temp1 = np.linspace(start_value, end_value, num_elements_forward)
+        remappedDisp.extend(temp1)
+
+        start_value_backward = remappedDisp[-1]
+        end_value_backward = 0
+        num_elements_backward = length - num_elements_forward
+
+        temp2 = np.linspace(start_value_backward, end_value_backward, num_elements_backward)
+        remappedDisp.extend(temp2)
+
+    for key, value in sorted_sample.items():
+        displacementS2.append(key)
+        resistanceS2.extend(value)
+
+    # Use linear interpolation to create a smooth curve
+    f_interp = interp1d(remappedDisp, resistanceS2, kind='linear', fill_value='extrapolate')
+
+    # Generate more x values for a smoother curve
+    x_interp = np.linspace(min(remappedDisp), max(remappedDisp), 1000)
+
+    # Calculate corresponding y values using the interpolation function
+    y_interp = f_interp(x_interp)
+
+    plt.plot(x_interp, y_interp, label='Smoothed Curve', color='red')
+    plt.scatter(remappedDisp, resistanceS2, label='Data Points', color='blue')
+
+    plt.title(plotTitle)
+    plt.xlabel('Displacement')
+    plt.ylabel('Resistance')
+    plt.legend()
+    plt.show()
+
+
+def PlotExp1Interp(FILEPATH, plotTitle):
 
     Sample = {}  # Key will be displacement and value, the readings
+    displacementS1 = []
+    resistanceS1 = []
+    # remappedDisp = []
 
     for values in os.listdir(FILEPATH):
         # RES_PATH = os.path.join(FILEPATH, values)
@@ -59,33 +207,101 @@ def PlotExp1Graph(FILEPATH, plotTitle):
     # Sort the dictionary items by key in ascending order
     sorted_sample = dict(sorted(Sample.items()))
 
-    # Extract x and y values from the sorted dictionary
-    x_values = np.array(list(sorted_sample.keys()))
-    y_values = np.array(list(sorted_sample.values()))
+    for key, value in sorted_sample.items():
+        # print(key, value)
+        displacementS1.append(key)
+        resistanceS1.extend(value)
 
-    # Flatten the y_values to a 1D array
-    y_values_flat = np.array([item for sublist in y_values for item in sublist])
+    div = 10/1240
+    # print(div)
 
-    # Repeat each x value according to the length of its corresponding y values
-    x_values_mapped = np.repeat(x_values, [len(sublist) for sublist in y_values])
+    # Specify the starting number, end value, and the increment
+    start_value = div
+    end_value = 10
+    num_elements = len(resistanceS1)
 
-    # Perform linear interpolation to make both arrays of the same length
-    f_interp = interp1d(x_values_mapped, y_values_flat, kind='linear', fill_value='extrapolate')
+    # Create the list using numpy's arange function
+    remappedDisp = np.linspace(start_value, end_value, num_elements).tolist()
+
+    print(len(remappedDisp))
+
+    # Use linear interpolation to create a smooth curve
+    f_interp = interp1d(remappedDisp, resistanceS1, kind='linear', fill_value='extrapolate')
 
     # Generate more x values for a smoother curve
-    x_interp = np.linspace(min(x_values_mapped), max(x_values_mapped), 1000)
+    x_interp = np.linspace(min(remappedDisp), max(remappedDisp), 1000)
 
     # Calculate corresponding y values using the interpolation function
     y_interp = f_interp(x_interp)
 
-    # Plot the original points as a continuous line
-    plt.plot(x_values_mapped, y_values_flat, 'o-', label='Original Data')
-
-    # Plot the smooth curve
     plt.plot(x_interp, y_interp, label='Smoothed Curve', color='red')
+    plt.scatter(remappedDisp, resistanceS1, label='Data Points', color='blue')
+
+    plt.title(plotTitle)
+    plt.xlabel('Displacement')
+    plt.ylabel('Resistance')
+    plt.legend()
+    plt.show()
+
+
+def PlotExp1Graph(FILEPATH, plotTitle):
+
+    Sample = {}  # Key will be displacement and value, the readings
+    displacementS1 = []
+    resistanceS1 = []
+    remappedDisp = []
+
+    for values in os.listdir(FILEPATH):
+        # RES_PATH = os.path.join(FILEPATH, values)
+
+        if len(values) == 7:
+            displacement_key = float(values[0])
+        elif len(values) == 8:
+            displacement_key = float(values[0:2])
+        elif len(values) == 9:
+            displacement_key = float(values[0:3])
+
+        # Handle the case where the file names do not include ".0"
+        if int(displacement_key) == displacement_key:
+            displacement_key = int(displacement_key)
+
+        Sample.setdefault(displacement_key, [])
+
+        with open(os.path.join(FILEPATH, values)) as file:
+            # file_contents = [float(line.strip()) for line in file.readlines()]
+            file_contents = [float(line.strip()) for line in file.readlines() if line.strip()]
+
+            Sample[displacement_key].extend(file_contents)
+
+    # Sort the dictionary items by key in ascending order
+    sorted_sample = dict(sorted(Sample.items()))
+
+    for key, value in sorted_sample.items():
+        # print(key, value)
+        displacementS1.append(key)
+        resistanceS1.extend(value)
+
+    div = 10/1240
+    # print(div)
+
+    # Specify the starting number, end value, and the increment
+    start_value = div
+    end_value = 10
+    num_elements = len(resistanceS1)
+
+    # Create the list using numpy's arange function
+    remappedDisp = np.linspace(start_value, end_value, num_elements).tolist()
+
+    print(len(remappedDisp))
+
+    # Extract x and y values from the sorted dictionary
+    x_values = np.array(remappedDisp)
+    y_values = np.array(resistanceS1)
+
+    plt.plot(x_values, y_values, label='Smoothed Curve', color='red')
 
     # Set specific values for the x-axis ticks
-    plt.xticks(np.unique(x_values))
+    plt.xticks(displacementS1)
     plt.yticks(np.unique(y_values))
 
     plt.title(plotTitle)
@@ -95,4 +311,17 @@ def PlotExp1Graph(FILEPATH, plotTitle):
     plt.show()
 
 
-PlotExp1Graph(EXP1SAM1PATH, f'Experiment 1 Sample 1')
+# Plotting Experiment 1 Samples
+counter = 1
+for plots in EXPERIMENT_1_FILES_PATH:
+    PlotExp1Graph(plots, f'Experiment 1 Sample {counter}')
+    PlotExp1Interp(plots, f'Experiment 1 Sample {counter}')
+    counter = counter + 1
+
+
+# Plotting Experiment 2 Samples
+counter = 1
+for plots in EXPERIMENT_2_FILES_PATH:
+    PlotExp2Graph(plots, f'Experiment 2 Sample {counter}')
+    PlotExp2Interp(plots, f'Experiment 2 Sample {counter}')
+    counter = counter + 1
